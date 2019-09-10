770c545bf156e972f7dc1f1fff512c0e667232b4
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2019-09-10 19:34:40 -0400

    added header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatMenuModule } from '@angular/material/menu';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DecksComponent } from './views/decks/decks.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
+
+let MATERIAL_COMPONENTS = [
+  MatMenuModule,
+  MatButtonModule
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +22,10 @@ import { DashboardComponent } from './views/dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
