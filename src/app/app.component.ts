f6ee9d71da1376ba504a5cf5ffdd01b33e72a8a8
Mauricio Alarcon <mauricio.alarcon1789@gmail.com>
2019-11-03 14:50:13 -0400

    added sign in and sign up buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+import { SessionService } from './services/session.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'flip-it-web';
+	constructor(
+		private router: Router,
+		private session: SessionService
+	) { }
+	title = 'flip-it-web';
+	goToLogin() {
+		window.location.href = this.session.getLoginUrl()
+	}
+	goToSignup() {
+		window.location.href = this.session.getSignupUrl()
+	}
 }
--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -4,6 +4,18 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SessionService {
-
-  constructor() { }
+	userPool = 'us-east-2_ZyTMDBKf1'
+	clientId = '1vbtqr9mbcusjtrd4cenhv9fgs'
+	authDomain = 'auth.mauridev.net'
+	redirectUrl = 'https://flip.mauridev.net'
+  	constructor() {}
+	cognitoUrl (action) {
+		return `https://${this.authDomain}/${action}?response_type=code&client_id=${this.clientId}&redirect_uri=${this.redirectUrl}`
+	}
+	getLoginUrl() {
+		return this.cognitoUrl('login')
+	}
+	getSignupUrl() {
+		return this.cognitoUrl('signup')
+	}
 }
